e477e135c81071d4ef951b1e6e5221632fc68d46
Admin01 <przemyslaw.grochal@duolife.eu>
2019-09-21 16:50:19 +0200

    v2

--- js/main.js
+++ js/main.js
@@ -2,20 +2,51 @@ const API = {
 
 	btnSearchFull: document.getElementById("btn-search-full"),
 	btnSearchId: document.getElementById("btn-search-id"),
+	// inputSearchSynch: document.getElementById("city​-synch"),
+
+	selectSynch:document.getElementById("select-synch"),
+
 	divEstate: document.getElementById("estate"),
 
-	showResult: function(res) {
+	completeSelect: function(res) {
+		// for (let i = 0; i < res.data.length; i++) {
+		// 	let option = document.createElement("option");
+		// 	option.text = res.data[i].city;
+		// 	API.selectSynch.add(option);
+		// }
 
-		function viewHelp(data) {
-			console.log(data);
-			console.log(data.id);
-			console.log(data.city);
-			
+		let id = 0;
+		for (let i = 0; i < res.data.length; i++) {
+			let option = document.createElement("option");
+			++id;
+			option.id = id;
+			option.text = res.data[i].city;
+
+			if(!document.getElementById(id)) {
+				API.selectSynch.add(option);	
+			}
+		}
+	},
+
+	showResult: function(res) {		
+		let html = '';
+		function buildHTML(data) {
+			html += `
+				<div class="estate-wrapper">
+					<div>${data.city}</div>
+					<div>${data.apartment}</div>
+					<div>${data.price}</div>
+					<div>${data.street}</div>
+				</div>
+			`;
+			return html;
 		}
 
 		for (let i = 0; i < res.data.length; i++) {
-			viewHelp(res.data[i]);
+			 buildHTML(res.data[i]);
 		}
+
+		API.divEstate.innerHTML = html;
 	},
 
 	setUrl: {
@@ -26,7 +57,7 @@ const API = {
 		delete: 'https://alfa.propertygrouppoland.pl/q/przemyslawgrochal/delete'
 	},
 
-	ajax: function(url, method) {
+	ajax: function(url, method, eventID) {
 		var xhttp = new XMLHttpRequest();
 		xhttp.open(method, url, true);
 
@@ -35,7 +66,14 @@ const API = {
 		    if (this.readyState == 4 && this.status == 200) {
 
 		        let res = JSON.parse(this.response);
-		        API.showResult(res);
+
+		        if(eventID === 0) {
+		        	API.showResult(res);
+		        }
+
+		        if(eventID === 1) {
+		        	API.completeSelect(res);
+		        }
 		    }
 		};
 
@@ -43,20 +81,27 @@ const API = {
 	},
 
 	checkAction: function(e) {
-		e.preventDefault();
-		
-		if(e.target.id === "btn-search-full") {
-			API.ajax(API.setUrl.getAll, 'GET');
-		}
+		if(e) {
+			e.preventDefault();
 
-		if(e.target.id === "btn-search-id") {
-			API.ajax(API.setUrl.getId, 'GET');
+			if(e.target.id === "btn-search-full") {
+				API.ajax(API.setUrl.getAll, 'GET', 0);
+			}
+
+			// if(e.target.id === "select-synch") {
+			// 	API.ajax(API.setUrl.getAll, 'GET', 1);
+			// }
 		}
+		
+		(function(){
+			API.ajax(API.setUrl.getAll, 'GET', 1);	
+		})();
 	},
 
 	initEvents: function() {
+		this.checkAction();
 		this.btnSearchFull.addEventListener("click", this.checkAction, false);
-		this.btnSearchId.addEventListener("click", this.checkAction, false);
+		this.selectSynch.addEventListener("change", this.checkAction, false);		
 	}
 }
 
